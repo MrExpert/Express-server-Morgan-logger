798e612e4e15ed96adf8840c580b2aed7cbe1a55
Sam Umirdinov <sam.umirdinov@gmail.com>
2019-04-20 17:19:15 -0700

    addes express & morgan to project

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 // import files and packages up here
+var express = require('express');
+var app = express();
+var morgan =require('morgan');
+var data = require('./data.json');
 
+app.use(morgan('tiny'));
 
 // create your express server below
-var app;
+app.get('/', function (req, res){
+    res.send("its working");
+});
+app.get('/data', function (req, res){
+    res.json(data);
+})
 
 // add your routes and middleware below
 
